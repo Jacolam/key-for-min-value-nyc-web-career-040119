4086e3466472a1085fdada5c13f879306a1bd940
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-19 23:05:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,9 @@
 # => :chair
 
 def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
-  lowest_key
+  
+  min = nil
+  
+  hash.each do |key, value|
+    if 
 end
